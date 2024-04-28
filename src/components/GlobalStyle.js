9a9c7bee2304f956a962430d488be2317f24c2ee
Flaperon09 <kasyanenko.yuriy@gmail.com>
2024-04-28 19:45:34 +0300

    Second commit

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,7 +1,10 @@
-@import-normalize; /* bring in normalize.css styles */
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
 
+export const GlobalStyle = createGlobalStyle `
 body {
   margin: 0;
+  background-color: #E7F2FF;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
@@ -13,3 +16,15 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+p, ul, section, h1, h2, h3 {
+  margin: 0;
+  padding: 0;
+}
+`;
